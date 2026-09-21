358b0c4cb2d101c564d3041553103a5ea35f9700
Ali <ali.bitcoin.cyber@gmail.com>
2026-09-21 07:41:55 +0430

    Bind native Office Ribbon UI to visible execution engine

--- src/OMNIX.Excel/OmnixRibbon.cs
+++ src/OMNIX.Excel/OmnixRibbon.cs
@@ -50,6 +50,8 @@ namespace OMNIX.Excel
         public void Ribbon_Load(Office.IRibbonUI ribbonUi)
         {
             _ribbonUi = ribbonUi;
+            var visibleHost = _addIn.Adapter as OMNIX.Core.Context.IVisibleOfficeExecutionHost;
+            if (visibleHost != null) visibleHost.BindRibbon(ribbonUi);
         }
 
         public bool GetVisible(Office.IRibbonControl control)
